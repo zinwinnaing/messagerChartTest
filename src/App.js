166892e2fa9a7f547d646a89544688cb99a3b7bf
Zin Win Naing (Achromex) <zinwinnaing@achromex.com>
2021-01-12 14:36:55 +0630

    fixed in index.html

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import MessengerCustomerChat from "react-messenger-customer-chat";
+// import MessengerCustomerChat from "react-messenger-customer-chat";
 
 function App() {
   return (
@@ -19,10 +19,10 @@ function App() {
           Learn React
         </a>
       </header>
-      <MessengerCustomerChat
+      {/* <MessengerCustomerChat
         pageId="103837605017893"
         appId="2450983945181800"
-      />
+      /> */}
     </div>
   );
 }
